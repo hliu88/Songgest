6d6196b0300c1c505bb8798b54435c76a4b95a84
hliu88 <gliu0529@gmail.com>
2021-10-06 18:21:42 -0400

    Update is_playlist_valid.py

--- app/is_playlist_valid.py
+++ app/is_playlist_valid.py
@@ -9,8 +9,6 @@ username = config('USERNAME')
 client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
 sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
-# playlist_url = 'https://open.spotify.com/playlist/114JM3RIWLBz7f4j1dTYjU'
-
 def is_valid(playlistURL):
 	try:
 		sp.user_playlist_tracks(username, playlist_url)
